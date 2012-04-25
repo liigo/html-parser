7f62de7e735f709c4a9be9d8dc62e4a5fced9da8
liigo <com.liigo@gmail.com>
2012-04-25 20:55:42 +0800

    add more TAG_*, and improve docs

--- HtmlParser.h
+++ HtmlParser.h
@@ -5,13 +5,15 @@
 #include <stdio.h>
 #include <stddef.h>
 
+// HtmlParser类，用于解析HTML文本
+// http://blog.csdn.net/liigo/article/details/6153829 (重要)
+// by liigo, @2010-2012
+
+
 namespace liigo
 {
 
-//HtmlParser类，用于解析HTML文本
-//by liigo, @2010-2012
-
-//MemBuffer: 内存缓冲区类，
+//MemBuffer: 内存缓冲区类，管理缓存区中连续存储的数据
 #define MEM_DEFAULT_BUFFER_SIZE  256
 class MemBuffer
 {
@@ -27,7 +29,7 @@ public:
 	//向缓存区复制数据块，写入现有数据的末尾，必要时会自动扩充缓存区
 	//返回新写入的数据块首地址在缓存区中的偏移量
 	size_t appendData(const void* pData, size_t nSize);
-	//取数据首地址(在数据长度为0时返回NULL)，此地址非NULL时也就是缓存区首地址
+	//取数据首地址(在数据长度为0时返回NULL)
 	//在appendXXX()或resetSize()或shrink()或exchange()或operator=调用之后可能会导致数据首地址发生改变
 	void* getData() const { return (m_nDataSize == 0 ? NULL : m_pBuffer); }
 	//取指定偏移处数据地址，偏移offset应小于getDataSize()，否则不保证返回的地址有效
@@ -77,19 +79,31 @@ private:
 enum HtmlNodeType
 {
 	NODE_UNKNOWN = 0,
-	NODE_START_TAG, //开始节点，如 <a href="liigo.com">
-	NODE_CLOSE_TAG, //结束节点，如 </a>
-	NODE_CONTENT,   //文本
-	NODE_REMARKS,   //注释 <!-- -->
+	NODE_START_TAG, //开始标签，如 <a href="liigo.com"> 或 <br/>
+	NODE_CLOSE_TAG, //结束标签，如 </a>
+	NODE_CONTENT,   //内容: 介于开始标签或结束标签之间的普通文本
+	NODE_REMARKS,   //注释: <!-- -->
 };
 
 enum HtmlTagType
 {
 	TAG_UNKNOWN = 0,
 	TAG_SCRIPT, TAG_STYLE, //出于解析需要必须识别,内部特别处理
-	TAG_A, TAG_B, TAG_BODY, TAG_BR, TAG_DIV, TAG_FONT, TAG_FORM, TAG_FRAME, TAG_FRAMESET, TAG_HR, 
-	TAG_I, TAG_IFRAME, TAG_INPUT, TAG_IMG, TAG_LABEL, TAG_LI, TAG_META, TAG_P, TAG_SPAN, TAG_TITLE, TAG_UL, 
-	//TAG_COLOR, TAG_BGCOLOR, //非标准HTML标签, 可以这样使用: <color=red>, 等效于 <color color=red>
+	//以下按标签字母顺序排列, 来源：http://www.w3school.com.cn/tags/
+	//此处仅定义类型值，不代表解析器可识别它，参见HtmlParser.onIdentifyHtmlTag()
+	TAG_A, TAG_ABBR, TAG_ACRONYM, TAG_ADDRESS, TAG_APPLET, TAG_AREA,
+	TAG_B, TAG_BASE,TAG_BASEFONT, TAG_BDO, TAG_BIG, TAG_BLOCKQUOTE, TAG_BODY, TAG_BR, TAG_BUTTON, 
+	TAG_CAPTION, TAG_CENTER, TAG_CITE, TAG_CODE, TAG_COL, TAG_COLGROUP, 
+	TAG_DD, TAG_DEL, TAG_DFN, TAG_DIR, TAG_DIV, TAG_DL, TAG_DT, TAG_EM, 
+	TAG_FONT, TAG_FORM, TAG_FRAME, TAG_FRAMESET, 
+	TAG_HEAD, TAG_H1, TAG_H2, TAG_H3, TAG_H4, H5, H6, TAG_HR, TAG_HTML, 
+	TAG_I, TAG_IFRAME, TAG_IMG, TAG_INPUT, TAG_INS, TAG_KBD, 
+	TAG_LABEL, TAG_LEGEND, TAG_LI, TAG_LINK, TAG_MAP, TAG_MENU, TAG_META, TAG_NOFRAMES, TAG_NOSCRIPT, 
+	TAG_OBJECT, TAG_OL, TAG_OPTGROUP, TAG_OPTION, TAG_P, TAG_PARAM, TAG_PRE, TAG_Q, 
+	TAG_S, TAG_SAMP, TAG_SELECT, TAG_SMALL, TAG_SPAN, TAG_STRIKE, TAG_STRONG, TAG_SUB, TAG_SUP, 
+	TAG_TABLE, TAG_TBODY, TAG_TD, TAG_TEXTAREA, TAG_TFOOT, TAG_TH, TAG_THEAD, TAG_TITLE, TAG_TR, TAG_TT, 
+	TAG_U, TAG_UL, TAG_VAR, 
+	_TAG_USER_TAG_, //用户定义的其他标签类型值应大于_TAG_USER_TAG_，以确保不与上面定义的常量值重复
 };
 
 enum HtmlNodeFlag
@@ -107,17 +121,21 @@ struct HtmlNodeProp
 
 struct HtmlNode
 {
-	HtmlNodeType type;
-	HtmlTagType  tagType;
-	char tagName[MAX_HTML_TAG_LENGTH+1];
-	char* text;
-	int propCount;
-	HtmlNodeProp* props;
-	size_t flags; //bit OR of HtmlNodeFlag
-	void* pUser; //user customized, default to NULL
+	HtmlNodeType type;    //节点类型
+	HtmlTagType  tagType; //标签类型（仅当节点类型为NODE_START_TAG或NODE_CLOSE_TAG时有意义）
+	char tagName[MAX_HTML_TAG_LENGTH+1]; //标签名称（如<A href="...">对应的标签名称为"A"）
+	char* text; //文本，视标签类型(tagType)不同意义也不同，可能为NULL
+				//如果type==NODE_START_TAG, text指向标签名称后面的属性文本
+				//如果type==NODE_CONTENT或NODE_REMARKS, text指向内容或注释文本
+	int propCount;        //属性个数（仅当属性被解析后有效，参见HtmlParser.parseNodeProps()，下同）
+	HtmlNodeProp* props;  //属性（名称=值），参见HtmlParser.getNodeProp()
+	size_t flags;    //bit OR of HtmlNodeFlag
+	char* pInternal; //reserved for internal usage
+	void* pUser;     //user customized, default to NULL
 };
 
 
+//HtmlParser: HTML文本解析类
 class HtmlParser
 {
 public:
@@ -138,19 +156,20 @@ public:
 	const char* getNodePropStringValue(const HtmlNode* pNode, const char* szPropName, const char* szDefaultValue = NULL);
 	int getNodePropIntValue(const HtmlNode* pNode, const char* szPropName, int defaultValue = 0);
 	void parseNodeProps(HtmlNode* pNode); //解析节点属性
-	//debug
+	//output
 	void outputHtmlNodes(FILE* f = stdout);
 	void outputHtml(MemBuffer& buffer, bool keepBufferData = false);
 
 protected:
-	//允许子类覆盖, 以便识别更多结点(提高解析质量), 或者识别更少结点(提高解析速度)
-	//默认仅识别涉及HTML基本结构和信息的有限几个TAG: A,IMG,META,BODY,TITLE,FRAME,IFRAME
+	//允许子类覆盖, 以便识别更多标签(提高解析质量), 或者识别更少标签(提高解析速度)
+	//默认仅识别涉及HTML基本结构和信息的有限几个开始标签: A,IMG,META,BODY,TITLE,FRAME,IFRAME
 	virtual HtmlTagType onIdentifyHtmlTag(const char* szTagName, HtmlNodeType nodeType);
 	//允许子类覆盖, 以便更好的解析节点属性, 或者部分解析甚至干脆不解析节点属性(提高解析速度)
-	//可以根据节点名称(pNode->tagName)或节点类型(pNode->tagType)判断是否需要解析属性
-	//默认仅解析已识别出节点类型的开始节点的属性（即pNode->type == NODE_START_TAG && pNode->tagType != NODE_UNKNOWN）
+	//可以根据标签名称(pNode->tagName)或标签类型(pNode->tagType)判断是否需要解析属性
+	//默认仅解析已识别出标签类型的开始标签的属性（即pNode->type == NODE_START_TAG && pNode->tagType != NODE_UNKNOWN）
 	virtual void onParseNodeProps(HtmlNode* pNode);
-	//允许子类覆盖, 在某节点解析完成后被调用，如果返回false则停止解析HTML
+	//允许子类覆盖, 在某节点解析完成后被调用, 如果返回false则立刻停止解析HTML
+	//这里也许是一个恰当的时机初始化pNode.pUser
 	virtual bool onNodeReady(HtmlNode* pNode) { return true; }
 
 private:
